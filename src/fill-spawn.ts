f68b1fccbd3c8d80af74d39e2721c731d533ee69
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-25 08:44:59 +0100

    Fix creep hanging if still has energy but spawn is full

--- src/fill-spawn.ts
+++ src/fill-spawn.ts
@@ -5,7 +5,7 @@ export function execute(creep: Creep): void {
     if (creep.transfer(Game.spawns[MAIN_SPAWN], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
         creep.moveTo(Game.spawns[MAIN_SPAWN]);
     }
-    if (creep.carry.energy === 0) {
+    if (creep.carry.energy === 0 || Game.spawns[MAIN_SPAWN].energy === Game.spawns[MAIN_SPAWN].energyCapacity) {
         creep.memory[MEMORY_TASK] = '';
     }
 }
