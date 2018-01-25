7bab5a71dce41d7dee8d6647baf12da37f2c4ce1
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-25 14:56:01 +0100

    Move creep logic into CreepRole class

--- src/creep-commanding.ts
+++ src/creep-commanding.ts
@@ -1,45 +1,21 @@
-import { MAIN_SPAWN, MEMORY_ROLE, MEMORY_TASK } from './constants';
-import * as ExtractEnergy from './extract-energy';
-import * as FillSpawn from './fill-spawn';
-import * as UpgradeController from './upgrade-controller';
+import { MEMORY_ROLE } from './constants';
+import { CreepRole } from './creeps/creep-role';
 
 export class CreepCommanding {
 
     public static execute() {
-        Object.keys(Game.creeps).map((it) => Game.creeps[it]).forEach((it) => {
-            const role = it.memory[MEMORY_ROLE];
-            if (role === 'harvester') {
-                if (!it.memory[MEMORY_TASK]) {
-                    if (it.carry.energy === it.carryCapacity) {
-                        if (Game.spawns[MAIN_SPAWN].energy === Game.spawns[MAIN_SPAWN].energyCapacity) {
-                            it.memory[MEMORY_TASK] = 'upgrade-controller';
-                        } else {
-                            it.memory[MEMORY_TASK] = 'fill-spawn';
-                        }
-                    } else {
-                        it.memory[MEMORY_TASK] = 'extract-energy';
-                    }
+        Object.keys(Game.creeps)
+            .map((it) => Game.creeps[it])
+            .forEach((it) => {
+                const role = it.memory[MEMORY_ROLE];
+                if (role === 'harvester') {
+                    new CreepRole(it).work();
+                } else if (role) {
+                    console.error(`${it.name} has unsupported role ${role}`);
+                } else {
+                    console.error(`${it.name} has no role assigned!`);
                 }
-                console.log(it.memory[MEMORY_TASK]);
-                switch (it.memory[MEMORY_TASK]) {
-                    case 'extract-energy':
-                        ExtractEnergy.execute(it);
-                        break;
-                    case 'fill-spawn':
-                        FillSpawn.execute(it);
-                        break;
-                    case 'upgrade-controller':
-                        UpgradeController.execute(it);
-                        break;
-                    default:
-                        console.log(`Behaviour for task ${it.memory[MEMORY_TASK]} is undefined`);
-                }
-            } else if (role) {
-                console.error(`${it.name} has unsupported role ${role}`);
-            } else {
-                console.error(`${it.name} has no role assigned!`);
-            }
-        });
+            });
     }
 
 }
---
+++ src/creeps/creep-role.ts
@@ -0,0 +1,65 @@
+
+import { MAIN_SPAWN } from '../constants';
+
+import * as ExtractEnergy from '../extract-energy';
+import * as FillSpawn from '../fill-spawn';
+import * as UpgradeController from '../upgrade-controller';
+
+const MEMORY = {
+    TASK: 'task'
+};
+
+export class CreepRole {
+
+    constructor(
+        private creep: Creep,
+    ) {
+    }
+
+    get currentTask(): string {
+        return this.creep.memory[MEMORY.TASK];
+    }
+
+    set currentTask(newTask: string) {
+        this.creep.memory[MEMORY.TASK] = newTask;
+    }
+
+    public work(): void {
+        const task = this.getTaskToExecute();
+        switch (task) {
+            case 'extract-energy':
+                ExtractEnergy.execute(this.creep);
+                break;
+            case 'fill-spawn':
+                FillSpawn.execute(this.creep);
+                break;
+            case 'upgrade-controller':
+                UpgradeController.execute(this.creep);
+                break;
+            default:
+                console.log(`Behaviour for task ${task} is undefined`);
+        }
+    }
+
+    private getTaskToExecute(): string {
+        let task = this.currentTask;
+        if (!task) {
+            task = this.findNewTask();
+            this.currentTask = task;
+        }
+        return task;
+    }
+
+    private findNewTask(): string {
+        if (this.creep.carry.energy === this.creep.carryCapacity) {
+            if (Game.spawns[MAIN_SPAWN].energy === Game.spawns[MAIN_SPAWN].energyCapacity) {
+                return 'upgrade-controller';
+            } else {
+                return 'fill-spawn';
+            }
+        } else {
+            return 'extract-energy';
+        }
+    }
+
+}
