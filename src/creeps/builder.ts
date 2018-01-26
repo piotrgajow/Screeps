797429dbb84e14cd478de7651dd397dcb51dbe79
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-26 23:24:49 +0100

    Optimize task choosing

--- src/creeps/builder.ts
+++ src/creeps/builder.ts
@@ -5,14 +5,12 @@ import { CreepRole } from './creep-role';
 export class Builder extends CreepRole {
 
     protected findNewTask(): string {
-        if (this.creep.carry.energy === this.creep.carryCapacity) {
-            if (ROOM.findConstructionSites(this.creep.room).length > 0) {
-                return 'build';
-            } else {
-                return 'repair';
-            }
-        } else {
+        if (this.creep.carry.energy === 0) {
             return 'extract-energy';
+        } else if (ROOM.findConstructionSites(this.creep.room).length > 0) {
+            return 'build';
+        } else {
+            return 'repair';
         }
     }
 
--- src/creeps/harvester.ts
+++ src/creeps/harvester.ts
@@ -1,21 +1,19 @@
 import COMMON from '../common';
 
 import { CreepRole } from './creep-role';
-import {ROOM} from '../room-utils';
+import { ROOM } from '../room-utils';
 
 export class Harvester extends CreepRole {
 
     protected findNewTask(): string {
-        if (this.creep.carry.energy === this.creep.carryCapacity) {
-            if (COMMON.MAIN_SPAWN.energy < COMMON.MAIN_SPAWN.energyCapacity) {
-                return 'fill-spawn';
-            } else if (_.any(ROOM.findExtensions(this.creep.room), Harvester.extensionNotFull)) {
-                return 'fill-extensions';
-            } else {
-                return 'upgrade-controller';
-            }
-        } else {
+        if (this.creep.carry.energy === 0) {
             return 'extract-energy';
+        } else if (COMMON.MAIN_SPAWN.energy < COMMON.MAIN_SPAWN.energyCapacity) {
+            return 'fill-spawn';
+        } else if (_.any(ROOM.findExtensions(this.creep.room), Harvester.extensionNotFull)) {
+            return 'fill-extensions';
+        } else {
+            return 'upgrade-controller';
         }
     }
 
