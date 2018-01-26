73db1b8a16880955eb6085460cf15711e8a2c70c
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-26 12:59:42 +0100

    Add spawning builders

--- src/creep-spawning.ts
+++ src/creep-spawning.ts
@@ -4,13 +4,38 @@ import COMMON from './common';
 export class CreepSpawning {
 
     public static execute(): void {
-        if (Object.keys(Game.creeps).length < 5) {
-            COMMON.MAIN_SPAWN.spawnCreep(
-                [WORK, WORK, CARRY, MOVE],
-                `Harvester${CreepSpawning.creepIndex()}`,
-                { memory: { role: 'harvester' } }
-            );
+        const TARGETS = {
+            builder: 2,
+            harvester: 5,
+        };
+
+        const creepRoleCounts = Object.keys(Game.creeps)
+            .map((it) => Game.creeps[it])
+            .reduce((iter, current) => {
+                const role = current.memory[COMMON.MEMORY.CREEP.ROLE];
+                iter[role] = iter[role] + 1;
+                return iter;
+            }, { builder: 0, harvester: 0 });
+
+        const roleToBuild = Object.keys(TARGETS).find((role) => {
+            return creepRoleCounts[role] < TARGETS[role];
+        });
+        if (roleToBuild) {
+            CreepSpawning.spawn(roleToBuild);
         }
+    }
+
+    private static spawn(role: string): void {
+        const DEFINITIONS = {
+            builder: { parts: [WORK, WORK, CARRY, MOVE], name: 'Builder' },
+            harvester: { parts: [WORK, WORK, CARRY, MOVE], name: 'Harvester' },
+        };
+        const definition = DEFINITIONS[role];
+        COMMON.MAIN_SPAWN.spawnCreep(
+            definition.parts,
+            `${definition.name}-${CreepSpawning.creepIndex()}`,
+            { memory: { role } },
+        );
     }
 
     private static creepIndex(): number {
