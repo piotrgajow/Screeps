d0d567178ffa71a0e3b4f315e7deabbebaa81810
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-27 23:52:19 +0100

    Rework repair task to use target

--- src/creeps/tasks/repair.ts
+++ src/creeps/tasks/repair.ts
@@ -1,3 +1,4 @@
+import { MEMORY } from '../../memory';
 import { ROOM } from '../../room-utils';
 
 import { Task } from './task';
@@ -5,19 +6,25 @@ import { Task } from './task';
 export class Repair extends Task {
 
     public initialize(creep: Creep): void {
+        creep.memory[MEMORY.TARGET] = this.findStructureToRepair(creep).id;
     }
 
     protected executeTask(creep: Creep): any {
-        const structures = ROOM.findStructures(creep.room);
-        const damagedStructures = structures.filter((structure) => structure.hits < structure.hitsMax);
-        const target = _.min(damagedStructures, (it) => it.hits);
+        const target = Game.getObjectById(creep.memory[MEMORY.TARGET]) as Structure;
         if (creep.repair(target) === ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
+        return target;
     }
 
     protected isTaskFinished(creep: Creep, opts: any): boolean {
-        return creep.carry.energy === 0;
+        return creep.carry.energy === 0 && (opts.hits === opts.hitsMax);
+    }
+
+    private findStructureToRepair(creep: Creep): any {
+        const structures = ROOM.findStructures(creep.room);
+        const damagedStructures = structures.filter((structure) => structure.hits < structure.hitsMax);
+        return _.min(damagedStructures, (it) => it.hits);
     }
 
 }
