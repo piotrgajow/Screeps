165f46dafe283aae6955e0f244e23c3579501646
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-28 19:57:29 +0100

    Add basic url in logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,11 @@
 
-interface NamedObject {
+interface LogObject {
+    id: string;
     name: string;
 }
 
-interface NamedObjectWithMemory {
+interface LogObjectWithMemory {
+    id: string;
     name: string;
     memory: object;
 }
@@ -18,26 +20,33 @@ export class Logger {
 
     private static MEMORY_KEY = 'debug';
 
-    public static debug(object: NamedObjectWithMemory, description: string): void {
+    public static debug(object: LogObjectWithMemory, description: string): void {
         if (object.memory[Logger.MEMORY_KEY]) {
             const message = this.formatMessage(LogLevel.DEBUG, object, description);
             console.log(message);
         }
     }
 
-    public static log(object: NamedObject, description: string): void {
+    public static log(object: LogObject, description: string): void {
         const message = this.formatMessage(LogLevel.LOG, object, description);
         console.log(message);
     }
 
-    public static error(object: NamedObject, description: string): void {
+    public static error(object: LogObject, description: string): void {
         const message = this.formatMessage(LogLevel.ERROR, object, description);
         console.log(message);
         Game.notify(message, 15);
     }
 
-    private static formatMessage(logLevel: LogLevel, object: NamedObject, message: string): string {
-        return `${logLevel} ${object.name}: ${message}`;
+    private static formatMessage(logLevel: LogLevel, object: LogObject, message: string): string {
+        const objectUrl = this.buildUrl(object.id, object.name);
+        return `${logLevel} ${objectUrl}: ${message}`;
+    }
+
+    private static buildUrl(id: string, text: string): string {
+        const onclick = `angular.element('body').injector().get('RoomViewPendingSelector').set('${id}');`;
+        const style = 'color: #428bca; cursor: pointer;';
+        return `<span style="${style}" onclick="${onclick}">${text}</span>`;
     }
 
 }
