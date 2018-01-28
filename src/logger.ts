742b94c23fc139662bc237c3bbdcd2b7d2ebcd29
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-28 17:01:58 +0100

    Improve logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,43 @@
 
+interface NamedObject {
+    name: string;
+}
+
+interface NamedObjectWithMemory {
+    name: string;
+    memory: object;
+}
+
+enum LogLevel {
+    DEBUG = '[DEBUG]',
+    LOG = '[LOG]',
+    ERROR = '[ERROR]',
+}
+
 export class Logger {
 
     private static MEMORY_KEY = 'debug';
 
-    public static debug(creep: Creep, message: string): void {
-        if (creep.memory[Logger.MEMORY_KEY]) {
-            console.log(`[DEBUG] ${creep.name}: ${message}`);
+    public static debug(object: NamedObjectWithMemory, description: string): void {
+        if (object.memory[Logger.MEMORY_KEY]) {
+            const message = this.formatMessage(LogLevel.DEBUG, object, description);
+            console.log(message);
         }
+    }
+
+    public static log(object: NamedObject, description: string): void {
+        const message = this.formatMessage(LogLevel.LOG, object, description);
+        console.log(message);
+    }
+
+    public static error(object: NamedObject, description: string): void {
+        const message = this.formatMessage(LogLevel.ERROR, object, description);
+        console.error(message);
+        Game.notify(message, 15);
+    }
+
+    private static formatMessage(logLevel: LogLevel, object: NamedObject, message: string): string {
+        return `${logLevel} ${object.name}: ${message}`;
     }
 
 }
