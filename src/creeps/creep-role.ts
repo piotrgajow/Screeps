ecfd598e3fdd0b21c8adfdaedc72506a1350b02e
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-28 13:42:15 +0100

    Improve logging

--- src/creeps/creep-role.ts
+++ src/creeps/creep-role.ts
@@ -1,5 +1,6 @@
 import COMMON from '../common';
 import { Logger } from '../logger';
+import { MEMORY } from '../memory';
 
 export abstract class CreepRole {
 
@@ -7,16 +8,16 @@ export abstract class CreepRole {
 
     public work(): void {
         const task = this.getTaskToExecute();
-        Logger.debug(this.creep, `Working on task '${task}'`);
+        Logger.debug(this.creep, `Working on task '${task}' with target '${this.creep.memory[MEMORY.TARGET]}'`);
         COMMON.TASKS[task].execute(this.creep);
     }
 
     private getTaskToExecute(): string {
-        let task = this.creep.memory[COMMON.MEMORY.CREEP.TASK];
+        let task = this.creep.memory[MEMORY.TASK];
         if (!task) {
             task = this.findNewTask();
             Logger.debug(this.creep, `New task '${task}' assigned`);
-            this.creep.memory[COMMON.MEMORY.CREEP.TASK] = task;
+            this.creep.memory[MEMORY.TASK] = task;
             COMMON.TASKS[task].initialize(this.creep);
         }
         return task;
