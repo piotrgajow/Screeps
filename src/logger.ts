8a0c70b13e9f2a80fe1b22c07296f08a2b5d4790
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-28 20:59:06 +0100

    Fix logger issues

--- src/logger.ts
+++ src/logger.ts
@@ -10,34 +10,34 @@ export class Logger {
 
     public static debug(logFlag: boolean, ...logObjects: Array<string | LogObject>): void {
         if (logFlag) {
-            this.printLog(LogLevel.DEBUG, logObjects);
+            Logger.printLog(LogLevel.DEBUG, logObjects);
         }
     }
 
     public static log(...logObjects: Array<string | LogObject>): void {
-        this.printLog(LogLevel.LOG, logObjects);
+        Logger.printLog(LogLevel.LOG, logObjects);
     }
 
     public static error(...logObjects: Array<string | LogObject>): void {
-        this.printLog(LogLevel.ERROR, logObjects);
+        Logger.printLog(LogLevel.ERROR, logObjects);
         // this.sendNotification(logObjects); // TODO
         // Game.notify(message, 15);
     }
 
     private static printLog(logLevel: LogLevel, logObjects: Array<string | LogObject>): void {
-        const message = this.buildMessage(logObjects);
+        const message = Logger.buildMessage(logObjects);
         console.log(`${logLevel} ${message}`);
     }
 
     private static buildMessage(logObjects: Array<string | LogObject>): string {
-        return logObjects.map(this.toString).join(' ');
+        return logObjects.map(Logger.toString).join(' ');
     }
 
     private static toString(logObject: string | LogObject): string {
         if (typeof logObject === 'string') {
             return logObject;
         } else {
-            return this.buildUrl(logObject);
+            return Logger.buildUrl(logObject);
         }
     }
 
