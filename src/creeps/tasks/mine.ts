170e4d8a1a707603b924d329f4bdf3db49fb8c36
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-30 18:58:32 +0100

    Rework mine task

--- src/creeps/tasks/mine.ts
+++ src/creeps/tasks/mine.ts
@@ -1,69 +1,45 @@
-import { Logger } from '../../logging/logger';
 import { MEMORY } from '../../memory';
-import { isMineFlag } from '../../structure-filters';
 
 import { Task } from './task';
 
 export class Mine extends Task {
 
     public initialize(creep: Creep): void {
-        creep.memory[MEMORY.TARGET] = Mine.findMineContainerId(creep);
+        creep.memory[MEMORY.TARGET] = this.findTarget(creep);
     }
 
-    protected executeTask(creep: Creep): any {
-        const mine = Game.getObjectById(creep.memory[MEMORY.TARGET]) as Structure;
-        creep.moveTo(mine);
-        return { mine };
-    }
-
-    protected isTaskFinished(creep: Creep, opts: any): boolean {
-        return creep.pos.toString() === opts.mine.pos.toString();
-    }
-
-    private static findMineContainerId(creep: Creep): string {
-        const mineFlags = creep.room.find(FIND_FLAGS, { filter: isMineFlag }) as Flag[];
-        const minerCreeps = _.transform(Game.creeps, creepMinerTransform, []) as Creep[];
-        const validMines = findValidMines(creep.room, mineFlags, minerCreeps);
-        switch (validMines.length) {
-            case 0:
-                Logger.error(creep.room.name, 'No empty mine found');
-                return '';
-            case 1:
-                return validMines[0].id;
-            default:
-                return creep.pos.findClosestByRange(validMines).id;
-        }
-    }
-
-}
-
-function findValidMines(room: Room, mineFlags: Flag[], minerCreeps: Creep[]): StructureContainer[] {
-    return _.reduce(mineFlags, (accumulator: StructureContainer[], flag: Flag) => {
-        const findResult = _.find(flag.pos.look(), (obj) => {
-            if (obj.type === 'structure') {
-                return obj.structure && obj.structure.structureType === STRUCTURE_CONTAINER;
-            }
-            return false;
-        });
-        if (findResult) {
-            const container = findResult.structure as StructureContainer;
-            const containerIsAlreadyAssigned = _.some(minerCreeps, (creep) => {
-                return creep.memory[MEMORY.TARGET] === container.id;
-            });
-            if (!containerIsAlreadyAssigned) {
-                accumulator.push(container);
-            }
-            return accumulator;
+    protected executeTask(creep: Creep): void {
+        const mineFlag = Game.flags[creep.memory[MEMORY.TARGET]];
+        if (mineFlag.pos.isEqualTo(creep.pos)) {
+            const source = Game.getObjectById(mineFlag.memory[MEMORY.SOURCE]) as Source;
+            creep.harvest(source);
         } else {
-            Logger.error(room.name, 'There is no container in position of mine flat', flag.name);
-            return accumulator;
+            creep.moveTo(mineFlag);
         }
-    }, []);
+    }
+
+    protected isTaskFinished(creep: Creep): boolean {
+        return false;
+    }
+
+    private findTarget(creep: Creep): string {
+        const mines = getAllMines();
+        const minerCreeps = findMinerCreeps();
+        const validMines = _.filter(mines, (mineFlagName) => {
+           return _.every(minerCreeps, (c) => {
+               return c.memory[MEMORY.TARGET] !== mineFlagName;
+           }) ;
+        });
+        const validMineFlags = _.map(validMines, (mineName) => Game.flags[mineName]);
+        return creep.pos.findClosestByPath(validMineFlags).name;
+    }
+
 }
 
-function creepMinerTransform(accumulator: Creep[], creep: Creep) {
-    if (creep.memory[MEMORY.ROLE] === 'miner') {
-        accumulator.push(creep);
-    }
-    return accumulator;
+function findMinerCreeps(): Creep[] {
+    return _.filter(_.values(Game.creeps), (creep) => creep.memory[MEMORY.ROLE] === 'miner');
+}
+
+function getAllMines(): string[] {
+    return Memory.mines;
 }
--- src/memory.ts
+++ src/memory.ts
@@ -1,6 +1,7 @@
 export enum MEMORY {
     DEBUG = 'debug',
     ROLE = 'role',
+    SOURCE = 'source',
     STORAGE = 'storage',
     TARGET = 'target',
     TASK = 'task',
