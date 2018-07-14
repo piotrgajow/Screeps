87b64e68e06df1967c3ca97da639629d6f25f1c0
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-07-14 10:36:57 +0200

    Add logging of game tick

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { ErrorMapper } from '_error-mapper/ErrorMapper';
 
+import { MEMORY } from './enums/memory';
+
+import { Logger } from './logging/logger';
+
 import { CreepCommanding } from './main-systems/creep-commanding';
 import { CreepSpawning } from './main-systems/creep-spawning';
 import { DefenceSystem } from './main-systems/defence-system';
@@ -8,6 +12,7 @@ import { MemoryCleanup } from './main-systems/memory-cleanup';
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
+    Logger.debug(Memory[MEMORY.DEBUG], '-'.repeat(25));
     DefenceSystem.execute();
     CreepCommanding.execute();
     CreepSpawning.execute();
