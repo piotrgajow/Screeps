dd72d2e97466093806accdecbdbc8b26bb84efeb
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-07-19 23:38:33 +0200

    Adjust remote hauler body composition

--- src/creeps/roles/remote-hauler.ts
+++ src/creeps/roles/remote-hauler.ts
@@ -5,6 +5,9 @@ import { CreepRole } from '../creep-role';
 
 export const REMOTE_HAULER = 'remote-hauler';
 
+const REMOTE_SOURCE_HARVEST_SPEED = 6;
+const CARRY_MODULE_CAPACITY = 100;
+
 export class RemoteHauler extends CreepRole {
 
     protected findNewTask(): string {
@@ -16,7 +19,15 @@ export class RemoteHauler extends CreepRole {
     }
 
     public getParts(room: Room): BodyPartConstant[] {
-        return [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE];
+        const combination = [CARRY, CARRY, MOVE];
+        const parts: BodyPartConstant[] = [];
+
+        // TODO calculate based on distance
+        const ticksDistance = 62;
+        const count = Math.ceil(ticksDistance * REMOTE_SOURCE_HARVEST_SPEED / CARRY_MODULE_CAPACITY);
+
+        combination.forEach((part) => _.times(count, () => parts.push(part)));
+        return parts;
     }
 
 }
